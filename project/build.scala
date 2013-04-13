813d36986be5528954006dfc636e6955b9017522
takezoe <takezoe@gmail.com>
2013-04-14 00:13:00 +0900

    Add scalatra-json and json4s dependencies.

--- project/build.scala
+++ project/build.scala
@@ -26,6 +26,8 @@ object MyBuild extends Build {
         "org.apache.commons" % "commons-io" % "1.3.2",
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "org.scalatra" %% "scalatra-json" % ScalatraVersion,
+        "org.json4s" %% "json4s-jackson" % "3.2.4",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
