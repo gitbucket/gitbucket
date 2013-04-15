6fe7e19cb1c6d5ea031b3d8566093f4af934890f
takezoe <takezoe@gmail.com>
2013-04-15 12:32:03 +0900

    Create GitBucketHome on boot if it does not exist.

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,10 +1,21 @@
 import app._
 import org.scalatra._
-import javax.servlet.ServletContext
+import javax.servlet._
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     context.mount(new CreateRepositoryServlet, "/new")
     context.mount(new RepositoryViewerServlet, "/*")
+    
+    context.addListener(new ServletContextListener(){
+      def contextInitialized(e: ServletContextEvent): Unit = {
+        val dir = new java.io.File(_root_.util.Directory.GitBucketHome)
+        if(!dir.exists){
+          dir.mkdirs()
+        }
+      }
+      
+      def contextDestroyed(e: ServletContextEvent): Unit = {}
+    })
   }
 }
