7c525931529b127e9870817b5fb60b0eb00be4d3
takezoe <takezoe@gmail.com>
2013-04-15 16:53:25 +0900

    Removed TODO and fixed indent.

--- src/main/webapp/assets/common/js/validation.js
+++ src/main/webapp/assets/common/js/validation.js
@@ -1,29 +1,28 @@
 $(function(){
-	$.each($('form[validate=true]'), function(i, form){
-		$(form).submit(validate);
-	});
+  $.each($('form[validate=true]'), function(i, form){
+    $(form).submit(validate);
+  });
 });
 
 function validate(e){
-	var form = $(e.target);
-	
-	if(form.data('validated') == true){
-		return true;
-	}
-	
-	// TODO use $.post() instead of $.getJSON
-	$.post(form.attr('action') + '/validate', $(e.target).serialize(), function(data){
-		// clear all error messages
-		$('.error-message').text('');
-		
-		if($.isEmptyObject(data)){
-			form.data('validated', true);
-			form.submit();
-		} else {
-			$.each(data, function(key, value){
-				$('#error-' + key).text(value);
-			});
-		}
-	}, 'json');
-	return false;
+  var form = $(e.target);
+  
+  if(form.data('validated') == true){
+    return true;
+  }
+
+  $.post(form.attr('action') + '/validate', $(e.target).serialize(), function(data){
+    // clear all error messages
+    $('.error-message').text('');
+    
+    if($.isEmptyObject(data)){
+      form.data('validated', true);
+      form.submit();
+    } else {
+      $.each(data, function(key, value){
+        $('#error-' + key).text(value);
+      });
+    }
+  }, 'json');
+  return false;
 }
