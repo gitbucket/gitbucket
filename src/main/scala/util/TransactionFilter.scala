2280b626a0f5ad303265a68dd982742d566b07bd
takezoe <takezoe@gmail.com>
2013-05-07 01:52:12 +0900

    Add TransactionFilter.

---
+++ src/main/scala/util/TransactionFilter.scala
@@ -0,0 +1,30 @@
+package util
+
+import javax.servlet._
+import org.slf4j.LoggerFactory
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * Controls the transaction with the open session in view pattern.
+ */
+class TransactionFilter extends Filter {
+  
+  private val logger = LoggerFactory.getLogger(classOf[TransactionFilter])
+  
+  def init(config: FilterConfig) = {}
+  
+  def destroy(): Unit = {}
+  
+  def doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain): Unit = {
+    if(req.asInstanceOf[HttpServletRequest].getRequestURI().startsWith("/assets/")){
+      // assets don't need transaction
+      chain.doFilter(req, res)
+    } else {
+      // TODO begin transaction!
+      logger.debug("TODO begin transaction")
+      chain.doFilter(req, res)
+      logger.debug("TODO end transaction")
+    }
+  }
+  
+}
