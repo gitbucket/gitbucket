63d76a3c6e9519b7ff639a10c73dfd8a7ace2b57
takezoe <takezoe@gmail.com>
2013-06-19 11:07:48 +0900

    Fix commit log presentation.

--- src/main/scala/view/helpers.scala
+++ src/main/scala/view/helpers.scala
@@ -15,27 +15,6 @@ object helpers {
    */
   def date(date: Date): String = new SimpleDateFormat("yyyy/MM/dd").format(date)
 
-  def format(value: String): Html =
-    Html(value.replaceAll(" ", "&nbsp;").replaceAll("\t", "&nbsp;&nbsp;&nbsp;&nbsp;").replaceAll("\n", "<br>"))
-
-  // TODO escape html tags using HtmlEscapeUtils (Commons Lang)
-  def formatCommitLog(message: String): Html = {
-    val i = message.trim.indexOf("\n")
-    val (firstLine, description) = if(i >= 0){
-      (message.trim.substring(0, i).trim, Some(message.trim.substring(i).trim))
-    } else {
-      (message.trim, None)
-    }
-
-    val sb = new StringBuilder()
-    sb.append("<div class=\"summary\">").append(format(firstLine).text).append("</div>")
-    if(description.isDefined){
-      sb.append("<div class=\"description\">").append(format(description.get).text).append("</div>")
-    }
-
-    Html(sb.toString)
-  }
-
   /**
    * Converts Markdown of Wiki pages to HTML.
    */
@@ -43,16 +22,5 @@ object helpers {
                enableWikiLink: Boolean, enableCommitLink: Boolean, enableIssueLink: Boolean)(implicit context: app.Context): Html = {
     Html(Markdown.toHtml(value, repository, enableWikiLink, enableCommitLink, enableIssueLink))
   }
-  
-//  /**
-//   * Cut the given string by specified length.
-//   */
-//  def cut(message: String, length: Int): String = {
-//    if(message.length > length){
-//      message.substring(0, length) + "..."
-//    } else {
-//      message
-//    }
-//  }
-  
+
 }
