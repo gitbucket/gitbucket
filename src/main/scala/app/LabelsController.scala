613e2a2be24866b8aa3eaeb343cfb6833b1aff6c
takezoe <takezoe@gmail.com>
2013-06-25 03:26:39 +0900

    Implementing label editing form.

--- src/main/scala/app/LabelsController.scala
+++ src/main/scala/app/LabelsController.scala
@@ -3,6 +3,7 @@ package app
 import jp.sf.amateras.scalatra.forms._
 import service._
 import util.WritableRepositoryAuthenticator
+import org.scalatra._
 
 class LabelsController extends LabelsControllerBase
   with LabelsService with RepositoryService with AccountService with WritableRepositoryAuthenticator
@@ -12,12 +13,17 @@ trait LabelsControllerBase extends ControllerBase {
 
   case class LabelForm(labelName: String, color: String)
 
-  val labelForm = mapping(
+  val newForm = mapping(
     "newLabelName" -> trim(label("Label name", text(required, maxlength(100)))),
     "newColor"     -> trim(label("Color",      text(required, maxlength(7))))
   )(LabelForm.apply)
 
-  post("/:owner/:repository/issues/label/new", labelForm)(writableRepository { form =>
+  val editForm = mapping(
+    "editLabelName" -> trim(label("Label name", text(required, maxlength(100)))),
+    "editColor"     -> trim(label("Color",      text(required, maxlength(7))))
+  )(LabelForm.apply)
+
+  post("/:owner/:repository/issues/label/new", newForm)(writableRepository { form =>
     val owner      = params("owner")
     val repository = params("repository")
 
@@ -35,6 +41,16 @@ trait LabelsControllerBase extends ControllerBase {
       case None    => NotFound()
       case Some(l) => issues.html.labeledit(Some(l), getRepository(owner, repository, baseUrl).get)
     }
+  })
+
+  post("/:owner/:repository/issues/label/:labelId/edit", editForm)(writableRepository { form =>
+    val owner      = params("owner")
+    val repository = params("repository")
+    val labelId    = params("labelId").toInt
+
+//    createLabel(owner, repository, form.labelName, form.color.substring(1))
+//
+    Ok("label updated.")
   })
 
 }
