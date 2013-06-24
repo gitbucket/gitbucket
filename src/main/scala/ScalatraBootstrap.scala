8a47dce97ea7548185267e3aa4c4db01aa85e087
takezoe <takezoe@gmail.com>
2013-06-24 14:18:32 +0900

    Change order of MilestonesController, LabelsController and IssuesController.

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -11,9 +11,9 @@ class ScalatraBootstrap extends LifeCycle {
     context.mount(new AccountController, "/*")
     context.mount(new RepositoryViewerController, "/*")
     context.mount(new WikiController, "/*")
-    context.mount(new IssuesController, "/*")
     context.mount(new LabelsController, "/*")
     context.mount(new MilestonesController, "/*")
+    context.mount(new IssuesController, "/*")
     context.mount(new SettingsController, "/*")
     
     context.addListener(new ServletContextListener(){
