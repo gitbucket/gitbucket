3edf195da88271204fdff69169d4c89c712c601d
takezoe <takezoe@gmail.com>
2013-06-27 21:15:15 +0900

    Fix controller order.

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -8,13 +8,13 @@ class ScalatraBootstrap extends LifeCycle {
     context.mount(new SignInController, "/*")
     context.mount(new UsersController, "/*")
     context.mount(new CreateRepositoryController, "/*")
+    context.mount(new AccountController, "/*")
     context.mount(new RepositoryViewerController, "/*")
     context.mount(new WikiController, "/*")
     context.mount(new LabelsController, "/*")
     context.mount(new MilestonesController, "/*")
     context.mount(new IssuesController, "/*")
     context.mount(new SettingsController, "/*")
-    context.mount(new AccountController, "/*")
 
     context.addListener(new ServletContextListener(){
       def contextInitialized(e: ServletContextEvent): Unit = {
