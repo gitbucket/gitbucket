1978061a06c0178fdbbb0c41e0bea7e781db6d41
takezoe <takezoe@gmail.com>
2013-07-10 00:16:55 +0900

    Display the message after settings updating is completed.

--- src/main/scala/app/SystemSettingsController.scala
+++ src/main/scala/app/SystemSettingsController.scala
@@ -4,11 +4,12 @@ import service.{AccountService, SystemSettingsService}
 import SystemSettingsService._
 import util.AdminAuthenticator
 import jp.sf.amateras.scalatra.forms._
+import org.scalatra.FlashMapSupport
 
 class SystemSettingsController extends SystemSettingsControllerBase
   with SystemSettingsService with AccountService with AdminAuthenticator
 
-trait SystemSettingsControllerBase extends ControllerBase {
+trait SystemSettingsControllerBase extends ControllerBase with FlashMapSupport {
   self: SystemSettingsService with AccountService with AdminAuthenticator =>
 
   private case class SystemSettingsForm(allowAccountRegistration: Boolean)
@@ -19,11 +20,12 @@ trait SystemSettingsControllerBase extends ControllerBase {
 
 
   get("/admin/system")(adminOnly {
-    admin.html.system(loadSystemSettings())
+    admin.html.system(loadSystemSettings(), flash.get("info"))
   })
 
   post("/admin/system", form)(adminOnly { form =>
     saveSystemSettings(SystemSettings(form.allowAccountRegistration))
+    flash += "info" -> "Settings updated."
     redirect("/admin/system")
   })
 
