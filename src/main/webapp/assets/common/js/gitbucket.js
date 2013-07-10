70761f4ac1bb58449944b7398b5944870457c017
takezoe <takezoe@gmail.com>
2013-07-10 21:09:21 +0900

    (refs #27)Display assigned user on issue list.

--- src/main/webapp/assets/common/js/gitbucket.js
+++ src/main/webapp/assets/common/js/gitbucket.js
@@ -1,11 +1,11 @@
 $(function(){
-  $.ajaxSetup({
-    cache: false
-  });
+  $.ajaxSetup({ cache: false });
 
   $('#repository-url').click(function(){
     this.select(0, this.value.length); 
   });
+
+  $('img[data-toggle=tooltip]').tooltip();
 
   prettyPrint();
 });
