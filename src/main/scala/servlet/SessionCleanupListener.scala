653872df8ec87e5835223210270a75f387f6c0fa
takezoe <takezoe@gmail.com>
2013-07-10 18:23:56 +0900

    (refs #28)Add SessionCleanupListener.

---
+++ src/main/scala/servlet/SessionCleanupListener.scala
@@ -0,0 +1,19 @@
+package servlet
+
+import util.FileUploadUtil
+import javax.servlet.http.{HttpSessionEvent, HttpSessionListener}
+import javax.servlet.annotation.WebListener
+
+/**
+ * Removes session associated temporary files when session is destroyed.
+ */
+class SessionCleanupListener extends HttpSessionListener {
+
+  def sessionCreated(se: HttpSessionEvent): Unit = {}
+
+  def sessionDestroyed(se: HttpSessionEvent): Unit = {
+    println("** session destroyed: " + se.getSession.getId)
+    FileUploadUtil.removeTemporaryFiles()(se.getSession)
+  }
+
+}
