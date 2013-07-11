07cdc6002d8c7683f7436a564f89ea1629f2c5ba
takezoe <takezoe@gmail.com>
2013-07-11 19:03:55 +0900

    Remove debug code.

--- src/main/scala/servlet/BasicAuthenticationFilter.scala
+++ src/main/scala/servlet/BasicAuthenticationFilter.scala
@@ -52,7 +52,6 @@ class BasicAuthenticationFilter extends Filter with RepositoryService with Accou
     } catch {
       case ex: Exception => {
         logger.error("error", ex)
-        ex.printStackTrace()
         requireAuth(response)
       }
     }
