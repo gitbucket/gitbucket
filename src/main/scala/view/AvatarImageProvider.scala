323e25951f699ce9a0a25d626d8e9a7fbf9a8185
takezoe <takezoe@gmail.com>
2013-07-18 13:40:21 +0900

    Use Gravatar if committer is not registered in GitBucket.

--- src/main/scala/view/AvatarImageProvider.scala
+++ src/main/scala/view/AvatarImageProvider.scala
@@ -10,11 +10,16 @@ trait AvatarImageProvider { self: RequestCache =>
    * Returns &lt;img&gt; which displays the avatar icon.
    * Looks up Gravatar if avatar icon has not been configured in user settings.
    */
-  protected def getAvatarImageHtml(userName: String, size: Int, tooltip: Boolean = false)(implicit context: app.Context): Html = {
+  protected def getAvatarImageHtml(userName: String, size: Int,
+      mailAddress: String = "", tooltip: Boolean = false)(implicit context: app.Context): Html = {
     val src = getAccountByUserName(userName).collect { case account if(account.image.isEmpty) =>
       s"""http://www.gravatar.com/avatar/${StringUtil.md5(account.mailAddress)}?s=${size}"""
     } getOrElse {
-      s"""${context.path}/${userName}/_avatar"""
+      if(mailAddress.nonEmpty){
+        s"""http://www.gravatar.com/avatar/${StringUtil.md5(mailAddress)}?s=${size}"""
+      } else {
+        s"""${context.path}/${userName}/_avatar"""
+      }
     }
     if(tooltip){
       Html(s"""<img src="${src}" class="avatar" style="width: ${size}px; height: ${size}px;" data-toggle="tooltip" title=${userName}/>""")
