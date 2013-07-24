b61836adf7cba0bd7f9edf4dcb175d3ce254d1d4
takezoe <takezoe@gmail.com>
2013-07-25 03:00:46 +0900

    Toggle Gravatar support at the system settings.

--- src/main/scala/view/AvatarImageProvider.scala
+++ src/main/scala/view/AvatarImageProvider.scala
@@ -12,15 +12,21 @@ trait AvatarImageProvider { self: RequestCache =>
    */
   protected def getAvatarImageHtml(userName: String, size: Int,
       mailAddress: String = "", tooltip: Boolean = false)(implicit context: app.Context): Html = {
-    val src = getAccountByUserName(userName).collect { case account if(account.image.isEmpty) =>
-      s"""http://www.gravatar.com/avatar/${StringUtil.md5(account.mailAddress)}?s=${size}"""
-    } getOrElse {
-      if(mailAddress.nonEmpty){
-        s"""http://www.gravatar.com/avatar/${StringUtil.md5(mailAddress)}?s=${size}"""
-      } else {
-        s"""${context.path}/${userName}/_avatar"""
+
+    val src = if(getSystemSettings().gravatar){
+      getAccountByUserName(userName).collect { case account if(account.image.isEmpty && !account.isGroupAccount) =>
+        s"""http://www.gravatar.com/avatar/${StringUtil.md5(account.mailAddress)}?s=${size}"""
+      } getOrElse {
+        if(mailAddress.nonEmpty){
+          s"""http://www.gravatar.com/avatar/${StringUtil.md5(mailAddress)}?s=${size}"""
+        } else {
+          s"""${context.path}/${userName}/_avatar"""
+        }
       }
+    } else {
+      s"""${context.path}/${userName}/_avatar"""
     }
+
     if(tooltip){
       Html(s"""<img src="${src}" class="avatar" style="width: ${size}px; height: ${size}px;" data-toggle="tooltip" title="${userName}"/>""")
     } else {
