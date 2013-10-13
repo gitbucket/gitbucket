7b2ee25ea2cd1d0c96e5e3d0b3e968c991aed785
HairyFotr <hairyfotr@gmail.com>
2013-10-13 21:16:10 +0200

    Add plugin, Update versions

--- project/build.scala
+++ project/build.scala
@@ -10,7 +10,7 @@ object MyBuild extends Build {
   val Organization = "jp.sf.amateras"
   val Name = "gitbucket"
   val Version = "0.0.1"
-  val ScalaVersion = "2.10.1"
+  val ScalaVersion = "2.10.3"
   val ScalatraVersion = "2.2.1"
 
   lazy val project = Project (
@@ -30,17 +30,17 @@ object MyBuild extends Build {
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "org.scalatra" %% "scalatra-json" % ScalatraVersion,
-        "org.json4s" %% "json4s-jackson" % "3.2.4",
+        "org.json4s" %% "json4s-jackson" % "3.2.5",
         "jp.sf.amateras" %% "scalatra-forms" % "0.0.2",
         "commons-io" % "commons-io" % "2.4",
-        "org.pegdown" % "pegdown" % "1.3.0",
+        "org.pegdown" % "pegdown" % "1.4.1",
         "org.apache.commons" % "commons-compress" % "1.5",
         "org.apache.commons" % "commons-email" % "1.3.1",
-        "org.apache.httpcomponents" % "httpclient" % "4.2.5",
+        "org.apache.httpcomponents" % "httpclient" % "4.3",
         "com.typesafe.slick" %% "slick" % "1.0.1",
         "com.novell.ldap" % "jldap" % "2009-10-07",
-        "com.h2database" % "h2" % "1.3.171",
-        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
+        "com.h2database" % "h2" % "1.3.173",
+        "ch.qos.logback" % "logback-classic" % "1.0.13" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container;provided",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar")),
         "junit" % "junit" % "4.11" % "test"
