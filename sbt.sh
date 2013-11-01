2e0e17f1aae692b4a0886fc4dd4ced87999e8c0e
takezoe <takezoe@gmail.com>
2013-11-02 05:12:36 +0900

    (refs #185)Add -Dsbt.log.noformat=true option

--- sbt.sh
+++ sbt.sh
@@ -1,1 +1,1 @@
-java -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx512M -Xss2M -jar `dirname $0`/sbt-launch-0.12.3.jar "$@"
+java -Dsbt.log.noformat=true -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx512M -Xss2M -jar `dirname $0`/sbt-launch-0.12.3.jar "$@"
