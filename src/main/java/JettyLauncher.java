6b11c1a180c28cf76577bc2cb93a324d54baac6a
takezoe <takezoe@gmail.com>
2013-11-30 05:22:35 +0900

    (refs #204)Change the option name from --data to --gitbucket.home

--- src/main/java/JettyLauncher.java
+++ src/main/java/JettyLauncher.java
@@ -27,7 +27,7 @@ public class JettyLauncher {
                         contextPath = dim[1];
                     } else if(dim[0].equals("--https") && (dim[1].equals("1") || dim[1].equals("true"))) {
                         forceHttps = true;
-                    } else if(dim[0].equals("--data")){
+                    } else if(dim[0].equals("--gitbucket.home")){
                         System.setProperty("gitbucket.home", dim[1]);
                     }
                 }
