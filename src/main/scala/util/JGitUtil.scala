94576a876a716818b6da89a3b2f6e9acb7c4da21
takezoe <takezoe@gmail.com>
2014-03-02 01:23:30 +0900

    (refs #280)Commit count limitation is changed to 10000 from 1000.

--- src/main/scala/util/JGitUtil.scala
+++ src/main/scala/util/JGitUtil.scala
@@ -128,7 +128,7 @@ object JGitUtil {
     using(Git.open(getRepositoryDir(owner, repository))){ git =>
       try {
         // get commit count
-        val commitCount = git.log.all.call.iterator.asScala.map(_ => 1).take(1000).sum
+        val commitCount = git.log.all.call.iterator.asScala.map(_ => 1).take(10000).sum
 
         RepositoryInfo(
           owner, repository, s"${baseUrl}/git/${owner}/${repository}.git",
