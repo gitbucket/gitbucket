7701521a2e0fe80986fc2a4b6c8f64ed7cd7b50b
takezoe <takezoe@gmail.com>
2014-03-02 04:24:24 +0900

    Fix to use Exception#ignoring to ignore Exception.

--- src/main/scala/util/ControlUtil.scala
+++ src/main/scala/util/ControlUtil.scala
@@ -16,7 +16,7 @@ object ControlUtil {
   def using[A <% { def close(): Unit }, B](resource: A)(f: A => B): B =
     try f(resource) finally {
       if(resource != null){
-        allCatch {
+        ignoring(classOf[Throwable]) {
           resource.close()
         }
       }
