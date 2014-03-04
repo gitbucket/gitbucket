f11be44c02414b85dbd0485f6f225586dc99205e
takezoe <takezoe@gmail.com>
2014-03-05 04:18:45 +0900

    (refs #330)Return NotFound if specified file does not exist

--- src/main/scala/app/RepositoryViewerController.scala
+++ src/main/scala/app/RepositoryViewerController.scala
@@ -82,44 +82,45 @@ trait RepositoryViewerControllerBase extends ControllerBase {
       val revCommit = JGitUtil.getRevCommitFromId(git, git.getRepository.resolve(id))
 
       @scala.annotation.tailrec
-      def getPathObjectId(path: String, walk: TreeWalk): ObjectId = walk.next match {
-        case true if(walk.getPathString == path) => walk.getObjectId(0)
-        case true => getPathObjectId(path, walk)
+      def getPathObjectId(path: String, walk: TreeWalk): Option[ObjectId] = walk.next match {
+        case true if(walk.getPathString == path) => Some(walk.getObjectId(0))
+        case true  => getPathObjectId(path, walk)
+        case false => None
       }
 
-      val objectId = using(new TreeWalk(git.getRepository)){ treeWalk =>
+      using(new TreeWalk(git.getRepository)){ treeWalk =>
         treeWalk.addTree(revCommit.getTree)
         treeWalk.setRecursive(true)
         getPathObjectId(path, treeWalk)
-      }
-
-      if(raw){
-        // Download
-        defining(JGitUtil.getContent(git, objectId, false).get){ bytes =>
-          contentType = FileUtil.getContentType(path, bytes)
-          bytes
-        }
-      } else {
-        // Viewer
-        val large  = FileUtil.isLarge(git.getRepository.getObjectDatabase.open(objectId).getSize)
-        val viewer = if(FileUtil.isImage(path)) "image" else if(large) "large" else "other"
-        val bytes  = if(viewer == "other") JGitUtil.getContent(git, objectId, false) else None
-
-        val content = if(viewer == "other"){
-          if(bytes.isDefined && FileUtil.isText(bytes.get)){
-            // text
-            JGitUtil.ContentInfo("text", bytes.map(StringUtil.convertFromByteArray))
-          } else {
-            // binary
-            JGitUtil.ContentInfo("binary", None)
+      } map { objectId =>
+        if(raw){
+          // Download
+          defining(JGitUtil.getContent(git, objectId, false).get){ bytes =>
+            contentType = FileUtil.getContentType(path, bytes)
+            bytes
           }
         } else {
-          // image or large
-          JGitUtil.ContentInfo(viewer, None)
-        }
+          // Viewer
+          val large  = FileUtil.isLarge(git.getRepository.getObjectDatabase.open(objectId).getSize)
+          val viewer = if(FileUtil.isImage(path)) "image" else if(large) "large" else "other"
+          val bytes  = if(viewer == "other") JGitUtil.getContent(git, objectId, false) else None
 
-        repo.html.blob(id, repository, path.split("/").toList, content, new JGitUtil.CommitInfo(revCommit))
-      }
+          val content = if(viewer == "other"){
+            if(bytes.isDefined && FileUtil.isText(bytes.get)){
+              // text
+              JGitUtil.ContentInfo("text", bytes.map(StringUtil.convertFromByteArray))
+            } else {
+              // binary
+              JGitUtil.ContentInfo("binary", None)
+            }
+          } else {
+            // image or large
+            JGitUtil.ContentInfo(viewer, None)
+          }
+
+          repo.html.blob(id, repository, path.split("/").toList, content, new JGitUtil.CommitInfo(revCommit))
+        }
+      } getOrElse NotFound
     }
   })
 
