cfc594805b7160230281d6959126cc1186c9be89
takezoe <takezoe@gmail.com>
2014-04-27 02:11:10 +0900

    (refs #348)Upgrade MINA to 0.11.0

--- project/build.scala
+++ project/build.scala
@@ -37,7 +37,7 @@ object MyBuild extends Build {
         "org.apache.commons" % "commons-compress" % "1.5",
         "org.apache.commons" % "commons-email" % "1.3.1",
         "org.apache.httpcomponents" % "httpclient" % "4.3",
-        "org.apache.sshd" % "apache-sshd" % "0.10.0",
+        "org.apache.sshd" % "apache-sshd" % "0.11.0",
         "com.typesafe.slick" %% "slick" % "1.0.1",
         "com.novell.ldap" % "jldap" % "2009-10-07",
         "com.h2database" % "h2" % "1.3.173",
