bbe455ac4998ca589feec9d53c3a809881292521
shimamoto <chibochibo@gmail.com>
2014-06-16 01:29:56 +0900

    (refs #341) Migrate model package.

---
+++ src/main/scala/model/Profile.scala
@@ -0,0 +1,20 @@
+package model
+
+import slick.driver.JdbcProfile
+
+//private[model]
+trait Profile {
+  val profile: JdbcProfile
+  import profile.simple._
+
+  // java.util.Date Mapped Column Types
+  implicit val dateColumnType = MappedColumnType.base[java.util.Date, java.sql.Timestamp](
+      d => new java.sql.Timestamp(d.getTime),
+      t => new java.util.Date(t.getTime)
+  )
+
+  implicit class RichColumn(c1: Column[Boolean]){
+    def &&(c2: => Column[Boolean], guard: => Boolean): Column[Boolean] = if(guard) c1 && c2 else c1
+  }
+
+}
--- src/main/scala/model/package.scala
+++ src/main/scala/model/package.scala
@@ -1,20 +1,22 @@
-package object model {
-  import scala.slick.driver.BasicDriver.Implicit._
-  import scala.slick.lifted.{Column, MappedTypeMapper}
+package object model extends {
+  // TODO
+  val profile = slick.driver.H2Driver
 
-  // java.util.Date TypeMapper
-  implicit val dateTypeMapper = MappedTypeMapper.base[java.util.Date, java.sql.Timestamp](
-      d => new java.sql.Timestamp(d.getTime),
-      t => new java.util.Date(t.getTime)
-  )
-
-  implicit class RichColumn(c1: Column[Boolean]){
-    def &&(c2: => Column[Boolean], guard: => Boolean): Column[Boolean] = if(guard) c1 && c2 else c1
-  }
-
+} with AccountComponent
+  with ActivityComponent
+  with CollaboratorComponent
+  with GroupMemberComponent
+  with IssueComponent
+  with IssueCommentComponent
+  with IssueLabelComponent
+  with LabelComponent
+  with MilestoneComponent
+  with PullRequestComponent
+  with RepositoryComponent
+  with SshKeyComponent
+  with WebHookComponent with Profile {
   /**
    * Returns system date.
    */
   def currentDate = new java.util.Date()
-
 }
