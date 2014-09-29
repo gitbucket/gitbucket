6fa7ea30fbbfe9fc8b8924d925330547f126721e
Tomofumi Tanaka <tanacasino@gmail.com>
2014-09-29 11:53:19 +0900

    (refs #498)Returns private own repository
    RepositoryService.getAllRepositories should returns
    private own repository too.

--- src/main/scala/service/RepositoryService.scala
+++ src/main/scala/service/RepositoryService.scala
@@ -176,6 +176,7 @@ trait RepositoryService { self: AccountService =>
   def getAllRepositories(userName: String)(implicit s: Session): List[(String, String)] = {
     Repositories.filter { t1 =>
       (t1.isPrivate === false.bind) ||
+        (t1.userName === userName.bind) ||
         (Collaborators.filter { t2 => t2.byRepository(t1.userName, t1.repositoryName) && (t2.collaboratorName === userName.bind)} exists)
     }.sortBy(_.lastActivityDate desc).map{ t =>
       (t.userName, t.repositoryName)
