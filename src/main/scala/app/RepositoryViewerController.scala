bc50b47d3acea30097d53888769df70fea67e530
Shintaro Murakami <mrkm4ntr@gmail.com>
2014-12-31 00:27:47 +0900

    (refs #584) Fix the activity of commenting to pull request.

--- src/main/scala/app/RepositoryViewerController.scala
+++ src/main/scala/app/RepositoryViewerController.scala
@@ -57,7 +57,7 @@ trait RepositoryViewerControllerBase extends ControllerBase {
     oldLineNumber: Option[Int],
     newLineNumber: Option[Int],
     content: String,
-    pullRequest: Boolean
+    issueId: Option[Int]
   )
 
   val editorForm = mapping(
@@ -83,7 +83,7 @@ trait RepositoryViewerControllerBase extends ControllerBase {
     "oldLineNumber" -> trim(label("Old line number", optional(number()))),
     "newLineNumber" -> trim(label("New line number", optional(number()))),
     "content"       -> trim(label("Content", text(required))),
-    "pullRequest"   -> trim(label("In pull request", boolean()))
+    "issueId"       -> trim(label("Issue Id", optional(number())))
   )(CommentForm.apply)
 
   /**
@@ -247,20 +247,23 @@ trait RepositoryViewerControllerBase extends ControllerBase {
   post("/:owner/:repository/commit/:id/comment/new", commentForm)(readableUsersOnly { (form, repository) =>
     val id = params("id")
     createCommitComment(repository.owner, repository.name, id, context.loginAccount.get.userName, form.content,
-      form.fileName, form.oldLineNumber, form.newLineNumber, form.pullRequest)
-    recordCommentCommitActivity(repository.owner, repository.name, context.loginAccount.get.userName, id, form.content)
+      form.fileName, form.oldLineNumber, form.newLineNumber, form.issueId.isDefined)
+    if (form.issueId.isDefined)
+      recordCommentPullRequestActivity(repository.owner, repository.name, context.loginAccount.get.userName, form.issueId.get, form.content)
+    else
+      recordCommentCommitActivity(repository.owner, repository.name, context.loginAccount.get.userName, id, form.content)
     redirect(s"/${repository.owner}/${repository.name}/commit/${id}")
   })
 
   ajaxGet("/:owner/:repository/commit/:id/comment/_form")(readableUsersOnly { repository =>
     val id            = params("id")
     val fileName      = params.get("fileName")
-    val oldLineNumber = params.get("oldLineNumber") flatMap {b => Some(b.toInt)}
-    val newLineNumber = params.get("newLineNumber") flatMap {b => Some(b.toInt)}
-    val pullRequest   = params.get("pullRequest")
+    val oldLineNumber = params.get("oldLineNumber") map (_.toInt)
+    val newLineNumber = params.get("newLineNumber") map (_.toInt)
+    val issueId   = params.get("issueId") map (_.toInt)
     repo.html.commentform(
       commitId = id,
-      fileName, oldLineNumber, newLineNumber, pullRequest.map(_.toBoolean).getOrElse(false),
+      fileName, oldLineNumber, newLineNumber, issueId,
       hasWritePermission = hasWritePermission(repository.owner, repository.name, context.loginAccount),
       repository = repository
     )
@@ -269,8 +272,11 @@ trait RepositoryViewerControllerBase extends ControllerBase {
   ajaxPost("/:owner/:repository/commit/:id/comment/_data/new", commentForm)(readableUsersOnly { (form, repository) =>
     val id = params("id")
     val commentId = createCommitComment(repository.owner, repository.name, id, context.loginAccount.get.userName,
-      form.content, form.fileName, form.oldLineNumber, form.newLineNumber, form.pullRequest)
-    recordCommentCommitActivity(repository.owner, repository.name, context.loginAccount.get.userName, id, form.content)
+      form.content, form.fileName, form.oldLineNumber, form.newLineNumber, form.issueId.isDefined)
+    if (form.issueId.isDefined)
+      recordCommentPullRequestActivity(repository.owner, repository.name, context.loginAccount.get.userName, form.issueId.get, form.content)
+    else
+      recordCommentCommitActivity(repository.owner, repository.name, context.loginAccount.get.userName, id, form.content)
     helper.html.commitcomment(getCommitComment(repository.owner, repository.name, commentId.toString).get,
       hasWritePermission(repository.owner, repository.name, context.loginAccount), repository)
   })
