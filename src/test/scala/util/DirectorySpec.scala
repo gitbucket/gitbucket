3057a31a6c8ed3f4e25f1112df6bcbdb6d093a60
nazoking <nazoking@gmail.com>
2015-02-26 11:47:47 +0900

    add test for Diretory (set GitBucketHome when test scope)

---
+++ src/test/scala/util/DirectorySpec.scala
@@ -0,0 +1,14 @@
+package util
+
+import org.specs2.mutable._
+
+class DirectorySpec extends Specification {
+  "GitBucketHome" should {
+    "set under target in test scope" in {
+      Directory.GitBucketHome mustEqual new java.io.File("target/gitbucket_home_for_test").getAbsolutePath
+    }
+    "exists" in {
+      new java.io.File(Directory.GitBucketHome).exists
+    }
+  }
+}
