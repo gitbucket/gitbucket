bc84cfc2c8b30af062f1eef81a8686a368cf2b37
Naoki Takezoe <takezoe@gmail.com>
2015-03-03 11:07:29 +0900

    Update assembly jar deploy script for 3.0 release

--- etc/deploy-assemby-jar.sh
+++ etc/deploy-assemby-jar.sh
@@ -2,8 +2,8 @@
 mvn deploy:deploy-file \
   -DgroupId=jp.sf.amateras\
   -DartifactId=gitbucket-assembly\
-  -Dversion=0.0.1\
+  -Dversion=3.0.0\
   -Dpackaging=jar\
-  -Dfile=../target/scala-2.11/gitbucket-assembly-0.0.1.jar\
+  -Dfile=../target/scala-2.11/gitbucket-assembly-3.0.0.jar\
   -DrepositoryId=sourceforge.jp\
   -Durl=scp://shell.sourceforge.jp/home/groups/a/am/amateras/htdocs/mvn/
