b39e863591b985af102998f963ca6595ec42a29a
nazoking <nazoking@gmail.com>
2015-03-29 01:12:05 +0900

    add test for getFileList

---
+++ src/test/scala/gitbucket/core/util/JGitUtilSpec.scala
@@ -0,0 +1,96 @@
+package gitbucket.core.util
+
+import org.specs2.mutable._
+import GitSpecUtil._
+
+class JGitUtilSpec extends Specification {
+
+  "getFileList(git: Git, revision: String, path)" should {
+    withTestRepository { git =>
+      def list(branch: String, path: String) = try {
+        JGitUtil.getFileList(git, branch, path).map(finfo => (finfo.name, finfo.message, finfo.isDirectory))
+      } catch {
+        case e: NullPointerException => Nil
+      }
+      list("master", ".") mustEqual Nil
+      list("master", "dir/subdir") mustEqual Nil
+      list("branch", ".") mustEqual Nil
+      list("branch", "dir/subdir") mustEqual Nil
+
+      createFile(git, "master", "README.md", "body1", message = "commit1")
+
+      list("master", ".") mustEqual List(("README.md", "commit1", false))
+      list("master", "dir/subdir") mustEqual Nil
+      list("branch", ".") mustEqual Nil
+      list("branch", "dir/subdir") mustEqual Nil
+
+      createFile(git, "master", "README.md", "body2", message = "commit2")
+
+      list("master", ".") mustEqual List(("README.md", "commit2", false))
+      list("master", "dir/subdir") mustEqual Nil
+      list("branch", ".") mustEqual Nil
+      list("branch", "dir/subdir") mustEqual Nil
+
+      createFile(git, "master", "dir/subdir/File3.md", "body3", message = "commit3")
+
+      list("master", ".") mustEqual List(("dir/subdir", "commit3", true), ("README.md", "commit2", false))
+      list("master", "dir/subdir") mustEqual List(("File3.md", "commit3", false))
+      list("branch", ".") mustEqual Nil
+      list("branch", "dir/subdir") mustEqual Nil
+
+      createFile(git, "master", "dir/subdir/File4.md", "body4", message = "commit4")
+
+      list("master", ".") mustEqual List(("dir/subdir", "commit4", true), ("README.md", "commit2", false))
+      list("master", "dir/subdir") mustEqual List(("File3.md", "commit3", false), ("File4.md", "commit4", false))
+      list("branch", ".") mustEqual Nil
+      list("branch", "dir/subdir") mustEqual Nil
+
+      createFile(git, "master", "README5.md", "body5", message = "commit5")
+
+      list("master", ".") mustEqual List(("dir/subdir", "commit4", true), ("README.md", "commit2", false), ("README5.md", "commit5", false))
+      list("master", "dir/subdir") mustEqual List(("File3.md", "commit3", false), ("File4.md", "commit4", false))
+      list("branch", ".") mustEqual Nil
+      list("branch", "dir/subdir") mustEqual Nil
+
+      createFile(git, "master", "README.md", "body6", message = "commit6")
+
+      list("master", ".") mustEqual List(("dir/subdir", "commit4", true), ("README.md", "commit6", false), ("README5.md", "commit5", false))
+      list("master", "dir/subdir") mustEqual List(("File3.md", "commit3", false), ("File4.md", "commit4", false))
+      list("branch", ".") mustEqual Nil
+      list("branch", "dir/subdir") mustEqual Nil
+
+      git.branchCreate().setName("branch").setStartPoint("master").call()
+
+      list("master", ".") mustEqual List(("dir/subdir", "commit4", true), ("README.md", "commit6", false), ("README5.md", "commit5", false))
+      list("master", "dir/subdir") mustEqual List(("File3.md", "commit3", false), ("File4.md", "commit4", false))
+      list("branch", ".") mustEqual List(("dir/subdir", "commit4", true), ("README.md", "commit6", false), ("README5.md", "commit5", false))
+      list("branch", "dir/subdir") mustEqual List(("File3.md", "commit3", false), ("File4.md", "commit4", false))
+
+      createFile(git, "branch", "dir/subdir/File3.md", "body7", message = "commit7")
+
+      list("master", ".") mustEqual List(("dir/subdir", "commit4", true), ("README.md", "commit6", false), ("README5.md", "commit5", false))
+      list("master", "dir/subdir") mustEqual List(("File3.md", "commit3", false), ("File4.md", "commit4", false))
+      list("branch", ".") mustEqual List(("dir/subdir", "commit7", true), ("README.md", "commit6", false), ("README5.md", "commit5", false))
+      list("branch", "dir/subdir") mustEqual List(("File3.md", "commit7", false), ("File4.md", "commit4", false))
+
+      createFile(git, "master", "dir8/File8.md", "body8", message = "commit8")
+
+      list("master", ".") mustEqual List(("dir/subdir", "commit4", true), ("dir8", "commit8", true), ("README.md", "commit6", false), ("README5.md", "commit5", false))
+      list("master", "dir/subdir") mustEqual List(("File3.md", "commit3", false), ("File4.md", "commit4", false))
+      list("branch", ".") mustEqual List(("dir/subdir", "commit7", true), ("README.md", "commit6", false), ("README5.md", "commit5", false))
+      list("branch", "dir/subdir") mustEqual List(("File3.md", "commit7", false), ("File4.md", "commit4", false))
+
+      createFile(git, "branch", "dir/subdir9/File9.md", "body9", message = "commit9")
+
+      list("master", ".") mustEqual List(("dir/subdir", "commit4", true), ("dir8", "commit8", true), ("README.md", "commit6", false), ("README5.md", "commit5", false))
+      list("master", "dir/subdir") mustEqual List(("File3.md", "commit3", false), ("File4.md", "commit4", false))
+      list("branch", ".") mustEqual List(("dir", "commit9", true), ("README.md", "commit6", false), ("README5.md", "commit5", false))
+      list("branch", "dir/subdir") mustEqual List(("File3.md", "commit7", false), ("File4.md", "commit4", false))
+
+      mergeAndCommit(git, "master", "branch", message = "merge10")
+
+      list("master", ".") mustEqual List(("dir", "commit9", true), ("dir8", "commit8", true), ("README.md", "commit6", false), ("README5.md", "commit5", false))
+      list("master", "dir/subdir") mustEqual List(("File3.md", "commit7", false), ("File4.md", "commit4", false))
+    }
+  }
+}
