7661e8caddf8c2b25d92b163a25c4a8ba91d96ed
Naoki Takezoe <takezoe@gmail.com>
2015-03-28 17:24:09 +0900

    Update assembly jar version to 3.1.0

--- etc/deploy-assemby-jar.sh
+++ etc/deploy-assemby-jar.sh
@@ -2,8 +2,8 @@
 mvn deploy:deploy-file \
   -DgroupId=gitbucket\
   -DartifactId=gitbucket-assembly\
-  -Dversion=3.0.0\
+  -Dversion=3.1.0\
   -Dpackaging=jar\
-  -Dfile=../target/scala-2.11/gitbucket-assembly-3.0.0.jar\
+  -Dfile=../target/scala-2.11/gitbucket-assembly-3.1.0.jar\
   -DrepositoryId=sourceforge.jp\
   -Durl=scp://shell.sourceforge.jp/home/groups/a/am/amateras/htdocs/mvn/
