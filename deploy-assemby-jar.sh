da3b7dbeff1441968fb0475cb6b0236644f56803
Naoki Takezoe <takezoe@gmail.com>
2015-05-07 11:14:17 +0900

    Improve assembly-jar deploying

--- deploy-assemby-jar.sh
+++ deploy-assemby-jar.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+./sbt.sh clean assembly
+
 mvn deploy:deploy-file \
   -DgroupId=gitbucket\
   -DartifactId=gitbucket-assembly\
   -Dversion=3.2.0\
   -Dpackaging=jar\
-  -Dfile=../target/scala-2.11/gitbucket-assembly-3.2.0.jar\
+  -Dfile=target/scala-2.11/gitbucket-assembly-3.2.0.jar\
   -DrepositoryId=sourceforge.jp\
   -Durl=scp://shell.sourceforge.jp/home/groups/a/am/amateras/htdocs/mvn/
