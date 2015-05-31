cbfae66882f9459916e7ab143ec02c06d32f95b8
Naoki Takezoe <takezoe@gmail.com>
2015-05-31 11:52:15 +0900

    Fix version

--- deploy-assembly/deploy-assembly-jar.sh
+++ deploy-assembly/deploy-assembly-jar.sh
@@ -4,7 +4,7 @@
 mvn deploy:deploy-file \
   -DgroupId=gitbucket\
   -DartifactId=gitbucket-assembly\
-  -Dversion=3.2.0\
+  -Dversion=3.3.0\
   -Dpackaging=jar\
   -Dfile=../target/scala-2.11/gitbucket-assembly-3.3.0.jar\
   -DrepositoryId=sourceforge.jp\
