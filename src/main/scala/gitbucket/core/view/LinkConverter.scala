a3daf13c15526f37d48b7de04dba89a7d49c5ee1
Naoki Takezoe <takezoe@gmail.com>
2015-09-07 02:10:25 +0900

    Fix issue link in Markdown

--- src/main/scala/gitbucket/core/view/LinkConverter.scala
+++ src/main/scala/gitbucket/core/view/LinkConverter.scala
@@ -18,47 +18,48 @@ trait LinkConverter { self: RequestCache =>
     escaped
       // convert username/project@SHA to link
       .replaceBy("(?<=(^|\\W))([a-zA-Z0-9\\-_]+)/([a-zA-Z0-9\\-_\\.]+)@([a-f0-9]{40})(?=(\\W|$))".r){ m =>
-      getAccountByUserName(m.group(2)).map { _ =>
-        s"""<a href="${context.path}/${m.group(2)}/${m.group(3)}/commit/${m.group(4)}">${m.group(2)}/${m.group(3)}@${m.group(4).substring(0, 7)}</a>"""
+        getAccountByUserName(m.group(2)).map { _ =>
+          s"""<a href="${context.path}/${m.group(2)}/${m.group(3)}/commit/${m.group(4)}">${m.group(2)}/${m.group(3)}@${m.group(4).substring(0, 7)}</a>"""
+        }
       }
-    }
 
       // convert username/project#Num to link
       .replaceBy( ("(?<=(^|\\W))([a-zA-Z0-9\\-_]+)/([a-zA-Z0-9\\-_\\.]+)" + issueIdPrefix + "([0-9]+)(?=(\\W|$))").r){ m =>
-      getIssue(m.group(2), m.group(3), m.group(4)) match {
-        case Some(issue) if (issue.isPullRequest)
-        => Some( s"""<a href="${context.path}/${m.group(2)}/${m.group(3)}/pull/${m.group(4)}">${m.group(2)}/${m.group(3)}#${m.group(4)}</a>""")
-        case Some(_) => Some( s"""<a href="${context.path}/${m.group(2)}/${m.group(3)}/issues/${m.group(4)}">${m.group(2)}/${m.group(3)}#${m.group(4)}</a>""")
-        case None => Some( s"""${m.group(2)}/${m.group(3)}#${m.group(4)}""")
+        getIssue(m.group(2), m.group(3), m.group(4)) match {
+          case Some(issue) if (issue.isPullRequest)
+          => Some( s"""<a href="${context.path}/${m.group(2)}/${m.group(3)}/pull/${m.group(4)}">${m.group(2)}/${m.group(3)}#${m.group(4)}</a>""")
+          case Some(_) => Some( s"""<a href="${context.path}/${m.group(2)}/${m.group(3)}/issues/${m.group(4)}">${m.group(2)}/${m.group(3)}#${m.group(4)}</a>""")
+          case None => Some( s"""${m.group(2)}/${m.group(3)}#${m.group(4)}""")
+        }
       }
-    }
 
       // convert username@SHA to link
       .replaceBy( ("(?<=(^|\\W))([a-zA-Z0-9\\-_]+)@([a-f0-9]{40})(?=(\\W|$))").r ) { m =>
-      getAccountByUserName(m.group(2)).map { _ =>
-        s"""<a href="${context.path}/${m.group(2)}/${repository.name}/commit/${m.group(3)}">${m.group(2)}@${m.group(3).substring(0, 7)}</a>"""
+        getAccountByUserName(m.group(2)).map { _ =>
+          s"""<a href="${context.path}/${m.group(2)}/${repository.name}/commit/${m.group(3)}">${m.group(2)}@${m.group(3).substring(0, 7)}</a>"""
+        }
       }
-    }
 
       // convert username#Num to link
       .replaceBy( ("(?<=(^|\\W))([a-zA-Z0-9\\-_]+)" + issueIdPrefix + "([0-9]+)(?=(\\W|$))").r ) { m =>
-      getIssue(m.group(2), repository.name, m.group(3)) match {
-        case Some(issue) if(issue.isPullRequest)
-        => Some(s"""<a href="${context.path}/${m.group(2)}/${repository.name}/pull/${m.group(3)}">${m.group(2)}#${m.group(3)}</a>""")
-        case Some(_) => Some(s"""<a href="${context.path}/${m.group(2)}/${repository.name}/issues/${m.group(3)}">${m.group(2)}#${m.group(3)}</a>""")
-        case None    => Some(s"""${m.group(2)}#${m.group(3)}""")
+        getIssue(m.group(2), repository.name, m.group(3)) match {
+          case Some(issue) if(issue.isPullRequest)
+          => Some(s"""<a href="${context.path}/${m.group(2)}/${repository.name}/pull/${m.group(3)}">${m.group(2)}#${m.group(3)}</a>""")
+          case Some(_) => Some(s"""<a href="${context.path}/${m.group(2)}/${repository.name}/issues/${m.group(3)}">${m.group(2)}#${m.group(3)}</a>""")
+          case None    => Some(s"""${m.group(2)}#${m.group(3)}""")
+        }
       }
-    }
 
       // convert issue id to link
       .replaceBy(("(?<=(^|\\W))(GH-|" + issueIdPrefix + ")([0-9]+)(?=(\\W|$))").r){ m =>
-      getIssue(repository.owner, repository.name, m.group(3)) match {
-        case Some(issue) if(issue.isPullRequest)
-        => Some(s"""<a href="${context.path}/${repository.owner}/${repository.name}/pull/${m.group(3)}">${m.group(2)}${m.group(3)}</a>""")
-        case Some(_) => Some(s"""<a href="${context.path}/${repository.owner}/${repository.name}/issues/${m.group(3)}">${m.group(2)}${m.group(3)}</a>""")
-        case None    => Some(s"""${m.group(2)}${m.group(3)}""")
+        val prefix = if(m.group(2) == "issue:") "#" else m.group(2)
+        getIssue(repository.owner, repository.name, m.group(3)) match {
+          case Some(issue) if(issue.isPullRequest)
+          => Some(s"""<a href="${context.path}/${repository.owner}/${repository.name}/pull/${m.group(3)}">${prefix}${m.group(3)}</a>""")
+          case Some(_) => Some(s"""<a href="${context.path}/${repository.owner}/${repository.name}/issues/${m.group(3)}">${prefix}${m.group(3)}</a>""")
+          case None    => Some(s"""${m.group(2)}${m.group(3)}""")
+        }
       }
-    }
 
       // convert @username to link
       .replaceBy("(?<=(^|\\W))@([a-zA-Z0-9\\-_\\.]+)(?=(\\W|$))".r){ m =>
