b64b447b4251f0533233d2c08f1f8d4c2202af53
Jannis Vamvas <vamvas@arteria.ch>
2015-10-26 14:30:49 +0100

    Extend API to allow creating repositories

---
+++ src/main/scala/gitbucket/core/api/CreateARepository.scala
@@ -0,0 +1,19 @@
+package gitbucket.core.api
+
+/**
+ * https://developer.github.com/v3/repos/#create
+ * api form
+ */
+case class CreateARepository(
+    name: String,
+    description: Option[String],
+    `private`: Boolean = false,
+    auto_init: Boolean = false
+) {
+  def isValid: Boolean = {
+    name.length<=40 &&
+        name.matches("[a-zA-Z0-9\\-\\+_.]+") &&
+        !name.startsWith("_") &&
+        !name.startsWith("-")
+  }
+}
