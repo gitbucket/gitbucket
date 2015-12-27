1081c0f16ce435a6a69610e1b45d220c9136a384
nazoking <nazoking@gmail.com>
2015-12-27 22:09:53 +0900

    remove blank line from basic-template

--- src/main/scala/gitbucket/core/model/BasicTemplate.scala
+++ src/main/scala/gitbucket/core/model/BasicTemplate.scala
@@ -54,7 +54,6 @@ protected[model] trait TemplateComponent { self: Profile =>
       byRepository(userName, repositoryName) && (this.commitId === commitId)
   }
 
-
   trait BranchTemplate extends BasicTemplate{ self: Table[_] =>
     val branch = column[String]("BRANCH")
     def byBranch(owner: String, repository: String, branchName: String) = byRepository(owner, repository) && (branch === branchName.bind)
