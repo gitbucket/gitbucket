5dad0777d7cad3c34e35dc42daf8de897e2cd0c7
Naoki Takezoe <takezoe@gmail.com>
2016-01-01 19:57:17 +0900

    Deploy assembly jar to snapshot repository if version ends with "-SNAPSHOT"

--- release/deploy-assembly-jar.sh
+++ release/deploy-assembly-jar.sh
@@ -5,6 +5,15 @@ cd ../
 ./sbt.sh clean assembly
 
 cd release
+
+if [[ "$GITBUCKET_VERSION" =~ -SNAPSHOT$ ]]; then
+  MVN_DEPLOY_PATH=mvn-snapshot
+else
+  MVN_DEPLOY_PATH=mvn
+fi
+
+echo $MVN_DEPLOY_PATH
+
 mvn deploy:deploy-file \
   -DgroupId=gitbucket\
   -DartifactId=gitbucket-assembly\
@@ -12,4 +21,4 @@ mvn deploy:deploy-file \
   -Dpackaging=jar\
   -Dfile=../target/scala-2.11/gitbucket-assembly-$GITBUCKET_VERSION.jar\
   -DrepositoryId=sourceforge.jp\
-  -Durl=scp://shell.sourceforge.jp/home/groups/a/am/amateras/htdocs/mvn/
+  -Durl=scp://shell.sourceforge.jp/home/groups/a/am/amateras/htdocs/$MVN_DEPLOY_PATH/
