c65e8434916862b661db79c762c0d0119a04133c
Naoki Takezoe <takezoe@gmail.com>
2016-05-26 19:01:06 +0900

    Use Travis provided DB in migration test

--- src/test/scala/gitbucket/core/GitBucketCoreModuleSpec.scala
+++ src/test/scala/gitbucket/core/GitBucketCoreModuleSpec.scala
@@ -27,50 +27,50 @@ class GitBucketCoreModuleSpec extends FunSuite {
   }
 
   test("Migration MySQL"){
-    val config = aMysqldConfig(v5_7_10)
-      .withPort(3306)
-      .withUser("gitbucket", "gitbucket")
-      .withCharset(Charset.UTF8)
-      .build()
-
-    val mysqld = anEmbeddedMysql(config)
-      .addSchema("gitbucket")
-      .start()
-
-    try {
+//    val config = aMysqldConfig(v5_7_10)
+//      .withPort(3306)
+//      .withUser("gitbucket", "gitbucket")
+//      .withCharset(Charset.UTF8)
+//      .build()
+//
+//    val mysqld = anEmbeddedMysql(config)
+//      .addSchema("gitbucket")
+//      .start()
+//
+//    try {
       new Solidbase().migrate(
-        DriverManager.getConnection("jdbc:mysql://localhost:3306/gitbucket", "gitbucket", "gitbucket"),
+        DriverManager.getConnection("jdbc:mysql://localhost:3306/gitbucket", "sa", "sa"),
         Thread.currentThread().getContextClassLoader(),
         new MySQLDatabase(),
         new Module(GitBucketCoreModule.getModuleId, GitBucketCoreModule.getVersions)
       )
-    } finally {
-      mysqld.stop()
-    }
+//    } finally {
+//      mysqld.stop()
+//    }
   }
 
   test("Migration PostgreSQL"){
-    val runtime = PostgresStarter.getDefaultInstance()
-    val config = new PostgresConfig(
-      PRODUCTION,
-      new Net("localhost", 5432),
-      new Storage("gitbucket"),
-      new Timeout(),
-      new Credentials("root", "root"))
-
-    val exec = runtime.prepare(config)
-    val process = exec.start()
-
-    try {
+//    val runtime = PostgresStarter.getDefaultInstance()
+//    val config = new PostgresConfig(
+//      PRODUCTION,
+//      new Net("localhost", 5432),
+//      new Storage("gitbucket"),
+//      new Timeout(),
+//      new Credentials("root", "root"))
+//
+//    val exec = runtime.prepare(config)
+//    val process = exec.start()
+//
+//    try {
       new Solidbase().migrate(
-        DriverManager.getConnection("jdbc:postgresql://localhost:5432/gitbucket", "root", "root"),
+        DriverManager.getConnection("jdbc:postgresql://localhost:5432/gitbucket", "sa", "sa"),
         Thread.currentThread().getContextClassLoader(),
         new PostgresDatabase(),
         new Module(GitBucketCoreModule.getModuleId, GitBucketCoreModule.getVersions)
       )
-    } finally {
-      process.stop()
-    }
+//    } finally {
+//      process.stop()
+//    }
   }
 
 }
