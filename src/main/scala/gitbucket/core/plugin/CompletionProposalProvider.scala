2e03f081d9e1ae1c2ced91689c9af3a7b86804c1
Naoki Takezoe <takezoe@gmail.com>
2016-07-13 01:56:43 +0900

    (refs #1241) Filter CompletionProposalProvider by the completion context

--- src/main/scala/gitbucket/core/plugin/CompletionProposalProvider.scala
+++ src/main/scala/gitbucket/core/plugin/CompletionProposalProvider.scala
@@ -9,6 +9,7 @@ trait CompletionProposalProvider {
   val prefix: String
   val suffix: String = " "
   val values: Seq[String]
+  val context: Seq[String]
 
   def template(implicit context: Context): String = "value"
   def additionalScript(implicit context: Context): String = ""
@@ -19,6 +20,7 @@ class EmojiCompletionProposalProvider extends CompletionProposalProvider {
   override val values: Seq[String] = EmojiUtil.emojis.toSeq
   override val prefix: String = ":"
   override val suffix: String = ": "
+  override val context: Seq[String] = Seq("wiki", "issues")
   override def template(implicit context: Context): String =
     s"""'<img src=\"${context.path}/assets/common/images/emojis/' + value + '.png\" class=\"emoji\"></img>' + value"""
 }
@@ -27,6 +29,7 @@ class UserCompletionProposalProvider extends CompletionProposalProvider {
   override val id: String = "user"
   override val values: Seq[String] = Nil
   override val prefix: String = "@"
+  override val context: Seq[String] = Seq("issues")
   override def template(implicit context: Context): String = "'@' + value"
   override def additionalScript(implicit context: Context): String =
     s"""$$.get('${context.path}/_user/proposals', { query: '' }, function (data) { user = data.options; });"""
