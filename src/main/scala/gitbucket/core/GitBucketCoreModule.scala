a1372034eda13bb7957ea647bd3429140e39af97
Naoki Takezoe <takezoe@gmail.com>
2016-09-22 11:37:37 +0900

    Ready for GitBucket 4.5 release

--- src/main/scala/gitbucket/core/GitBucketCoreModule.scala
+++ src/main/scala/gitbucket/core/GitBucketCoreModule.scala
@@ -14,5 +14,6 @@ object GitBucketCoreModule extends Module("gitbucket-core",
   ),
   new Version("4.2.1"),
   new Version("4.3.0"),
-  new Version("4.4.0")
+  new Version("4.4.0"),
+  new Version("4.5.0")
 )
