a7fda9b644b5b18f60a3ac74457074ba0db4cb31
johanprinsloo <johan.a.prinsloo@gmail.com>
2016-11-05 11:20:26 -0700

    Cross origin from simsci.io

--- src/main/scala/gitbucket/core/schneider/ConsumerConfig.scala
+++ src/main/scala/gitbucket/core/schneider/ConsumerConfig.scala
@@ -4,7 +4,7 @@ object ConsumerConfig {
   val consumerHost: String = System.getProperty("consumer.host") match {
     case host: String if (host.nonEmpty) => host
               // Dev "http://localhost:9000"
-    case _ => "https://portal.modeler.gy"
+    case _ => "https://dynsim.simsci.io"
   }
 
 }
