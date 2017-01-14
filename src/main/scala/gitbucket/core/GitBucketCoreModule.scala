53537eaa09c3f43213215bb4bd05a510e85884ce
nazoking <nazoking@gmail.com>
2017-01-15 04:11:14 +0900

    rewrite sql to LiquibaseMigration

--- src/main/scala/gitbucket/core/GitBucketCoreModule.scala
+++ src/main/scala/gitbucket/core/GitBucketCoreModule.scala
@@ -26,6 +26,6 @@ object GitBucketCoreModule extends Module("gitbucket-core",
   new Version("4.7.1"),
   new Version("4.8"),
   new Version("4.9",
-    new SqlMigration("update/gitbucket-core_4.9.sql")
+    new LiquibaseMigration("update/gitbucket-core_4.9.xml")
   )
 )
