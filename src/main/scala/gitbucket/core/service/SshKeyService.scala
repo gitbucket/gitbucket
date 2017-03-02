5a1ec385a8566504ce217159f4fa5e5887ef781f
Naoki Takezoe <takezoe@gmail.com>
2017-03-02 17:31:04 +0900

    (refs #474)Add controller to maintain deploy keys

--- src/main/scala/gitbucket/core/service/SshKeyService.scala
+++ src/main/scala/gitbucket/core/service/SshKeyService.scala
@@ -7,7 +7,7 @@ import gitbucket.core.model.Profile.profile.blockingApi._
 trait SshKeyService {
 
   def addPublicKey(userName: String, title: String, publicKey: String)(implicit s: Session): Unit =
-    SshKeys insert SshKey(userName = userName, title = title, publicKey = publicKey)
+    SshKeys.insert(SshKey(userName = userName, title = title, publicKey = publicKey))
 
   def getPublicKeys(userName: String)(implicit s: Session): List[SshKey] =
     SshKeys.filter(_.userName === userName.bind).sortBy(_.sshKeyId).list
@@ -16,6 +16,6 @@ trait SshKeyService {
     SshKeys.filter(_.publicKey.trim =!= "").list
 
   def deletePublicKey(userName: String, sshKeyId: Int)(implicit s: Session): Unit =
-    SshKeys filter (_.byPrimaryKey(userName, sshKeyId)) delete
+    SshKeys.filter(_.byPrimaryKey(userName, sshKeyId)).delete
 
 }
