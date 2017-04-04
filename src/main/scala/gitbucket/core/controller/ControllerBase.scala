956af54d4f9aec4d03341412670807619d42665a
Naoki Takezoe <takezoe@gmail.com>
2017-04-05 01:48:24 +0900

    Remove outdated TODO

--- src/main/scala/gitbucket/core/controller/ControllerBase.scala
+++ src/main/scala/gitbucket/core/controller/ControllerBase.scala
@@ -40,10 +40,6 @@ abstract class ControllerBase extends ScalatraFilter
     contentType = formats("json")
   }
 
-// TODO Scala 2.11
-//  // Don't set content type via Accept header.
-//  override def format(implicit request: HttpServletRequest) = ""
-
   override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = try {
     val httpRequest  = request.asInstanceOf[HttpServletRequest]
     val httpResponse = response.asInstanceOf[HttpServletResponse]
@@ -150,14 +146,6 @@ abstract class ControllerBase extends ScalatraFilter
         }
       }
     }
-
-  // TODO Scala 2.11
-  override def url(path: String, params: Iterable[(String, Any)] = Iterable.empty,
-                   includeContextPath: Boolean = true, includeServletPath: Boolean = true,
-                   absolutize: Boolean = true, withSessionId: Boolean = true)
-                  (implicit request: HttpServletRequest, response: HttpServletResponse): String =
-    if (path.startsWith("http")) path
-    else baseUrl + super.url(path, params, false, false, false)
 
   /**
    * Extends scalatra-form's trim rule to eliminate CR and LF.
