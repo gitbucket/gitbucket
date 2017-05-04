014bdb9d5c7cc60d483eafc8c60894d104e3fe5b
Naoki Takezoe <takezoe@gmail.com>
2017-05-04 10:29:48 +0900

    Bump to 4.12.1

--- src/main/scala/gitbucket/core/GitBucketCoreModule.scala
+++ src/main/scala/gitbucket/core/GitBucketCoreModule.scala
@@ -32,5 +32,6 @@ object GitBucketCoreModule extends Module("gitbucket-core",
   new Version("4.11.0",
     new LiquibaseMigration("update/gitbucket-core_4.11.xml")
   ),
-  new Version("4.12.0")
+  new Version("4.12.0"),
+  new Version("4.12.1")
 )
