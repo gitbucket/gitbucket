cab633e984e918fb3267f2da5490919a03a80c44
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-08-07 16:42:14 +0900

    add a script for creating a rpm file from war file.

---
+++ contrib/linux/redhat/createrpm.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "usage: $0 version"
+  echo "example: $0 4.15.0"
+  exit 1
+fi
+export VERSION=$1
+
+rm *.rpm
+rm -rf ~/rpmbuild
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+
+export currentdir=$(cd $(dirname $0); pwd)
+cd $currentdir
+rm   gitbucket.war
+wget https://github.com/gitbucket/gitbucket/releases/download/$VERSION/gitbucket.war
+
+cp gitbucket.war         ~/rpmbuild/SOURCES/
+cp gitbucket.init        ~/rpmbuild/SOURCES/
+cp ../../gitbucket.conf  ~/rpmbuild/SOURCES/
+sed "s/Version:\(\s\+\).*/Version:\1$VERSION/" gitbucket.spec > ~/rpmbuild/SPECS/gitbucket.spec
+
+cd ~
+rpmbuild -ba rpmbuild/SPECS/gitbucket.spec
+cp           rpmbuild/RPMS/noarch/gitbucket*.rpm $currentdir
+
+
+
