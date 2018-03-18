d8e5a89ac5bae20c58d24c164683ee715709f18a
Naoki Takezoe <takezoe@gmail.com>
2018-03-18 13:50:30 +0900

    Rename table RELEASE to RELEASE_TAG

--- src/main/scala/gitbucket/core/model/ReleaseTag.scala
+++ src/main/scala/gitbucket/core/model/ReleaseTag.scala
@@ -1,14 +1,14 @@
 package gitbucket.core.model
 
-trait ReleaseComponent extends TemplateComponent {
+trait ReleaseTagComponent extends TemplateComponent {
   self: Profile =>
 
   import profile.api._
   import self._
 
-  lazy val Releases = TableQuery[Releases]
+  lazy val ReleaseTags = TableQuery[ReleaseTags]
 
-  class Releases(tag_ : Tag) extends Table[Release](tag_, "RELEASE") with BasicTemplate {
+  class ReleaseTags(tag_ : Tag) extends Table[ReleaseTag](tag_, "RELEASE_TAG") with BasicTemplate {
     val name = column[String]("NAME")
     val tag = column[String]("TAG")
     val author = column[String]("AUTHOR")
@@ -16,13 +16,13 @@ trait ReleaseComponent extends TemplateComponent {
     val registeredDate = column[java.util.Date]("REGISTERED_DATE")
     val updatedDate = column[java.util.Date]("UPDATED_DATE")
 
-    def * = (userName, repositoryName, name, tag, author, content, registeredDate, updatedDate) <> (Release.tupled, Release.unapply)
+    def * = (userName, repositoryName, name, tag, author, content, registeredDate, updatedDate) <> (ReleaseTag.tupled, ReleaseTag.unapply)
     def byPrimaryKey(owner: String, repository: String, tag: String) = byTag(owner, repository, tag)
     def byTag(owner: String, repository: String, tag: String) = byRepository(owner, repository) && (this.tag === tag.bind)
   }
 }
 
-case class Release(
+case class ReleaseTag(
   userName: String,
   repositoryName: String,
   name: String,
