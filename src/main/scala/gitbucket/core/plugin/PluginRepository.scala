99f228bb9494ba170a0fde592fb4f649ac8fb099
Naoki Takezoe <takezoe@gmail.com>
2018-06-11 19:18:26 +0900

    Remove unused variables

--- src/main/scala/gitbucket/core/plugin/PluginRepository.scala
+++ src/main/scala/gitbucket/core/plugin/PluginRepository.scala
@@ -1,8 +1,7 @@
 package gitbucket.core.plugin
 
 import org.json4s._
-import gitbucket.core.util.Directory._
-import org.apache.commons.io.{FileUtils, IOUtils}
+import org.apache.commons.io.IOUtils
 
 object PluginRepository {
   implicit val formats = DefaultFormats
@@ -10,9 +9,6 @@ object PluginRepository {
   def parsePluginJson(json: String): Seq[PluginMetadata] = {
     org.json4s.jackson.JsonMethods.parse(json).extract[Seq[PluginMetadata]]
   }
-
-  lazy val LocalRepositoryDir = new java.io.File(PluginHome, ".repository")
-  lazy val LocalRepositoryIndexFile = new java.io.File(LocalRepositoryDir, "plugins.json")
 
   def getPlugins(): Seq[PluginMetadata] = {
     val url = new java.net.URL("https://plugins.gitbucket-community.org/releases/plugins.json")
