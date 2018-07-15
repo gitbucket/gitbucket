31345cc73925b19e2424ba5de570c050e7660d7b
Naoki Takezoe <takezoe@gmail.com>
2018-07-15 12:52:22 +0900

    Add proxy support for plugin installation

--- src/main/scala/gitbucket/core/plugin/PluginRepository.scala
+++ src/main/scala/gitbucket/core/plugin/PluginRepository.scala
@@ -1,7 +1,12 @@
 package gitbucket.core.plugin
 
+import java.net.{InetSocketAddress, PasswordAuthentication}
+import java.net.{Proxy => JProxy}
+
+import gitbucket.core.controller.Context
 import org.json4s._
 import org.apache.commons.io.IOUtils
+import java.net.Authenticator
 
 object PluginRepository {
   implicit val formats = DefaultFormats
@@ -10,9 +15,27 @@ object PluginRepository {
     org.json4s.jackson.JsonMethods.parse(json).extract[Seq[PluginMetadata]]
   }
 
-  def getPlugins(): Seq[PluginMetadata] = {
+  def getPlugins()(implicit context: Context): Seq[PluginMetadata] = {
     val url = new java.net.URL("https://plugins.gitbucket-community.org/releases/plugins.json")
-    val str = IOUtils.toString(url, "UTF-8")
+
+    val in = context.settings.proxy match {
+      case Some(proxy) =>
+        (proxy.user, proxy.password) match {
+          case (Some(user), Some(password)) =>
+            Authenticator.setDefault(new Authenticator() {
+              override def getPasswordAuthentication = new PasswordAuthentication(user, password.toCharArray)
+            })
+          case _ =>
+            Authenticator.setDefault(null)
+        }
+
+        url
+          .openConnection(new JProxy(JProxy.Type.HTTP, new InetSocketAddress(proxy.host, proxy.port)))
+          .getInputStream
+      case None => url.openStream()
+    }
+
+    val str = IOUtils.toString(in, "UTF-8")
     parsePluginJson(str)
   }
 
