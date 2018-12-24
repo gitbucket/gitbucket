dcf2f1dfdf698e44eef793d7f3683e2e4754a160
Naoki Takezoe <takezoe@gmail.com>
2018-12-24 23:59:14 +0900

    Create template of testcase for WebHook models JSON serialization

---
+++ src/test/scala/gitbucket/core/service/WebHookJsonFormatSpec.scala
@@ -0,0 +1,38 @@
+package gitbucket.core.service
+
+import org.json4s.jackson.JsonMethods.parse
+import org.json4s._
+import org.scalatest.FunSuite
+
+class WebHookJsonFormatSpec extends FunSuite {
+  import gitbucket.core.api.ApiSpecModels._
+
+  test("WebHookCreatePayload"){
+    fail("TODO")
+  }
+
+  test("WebHookPushPayload"){
+    fail("TODO")
+  }
+
+  test("WebHookIssuesPayload"){
+    fail("TODO")
+  }
+
+  test("WebHookPullRequestPayload"){
+    fail("TODO")
+  }
+
+  test("WebHookIssueCommentPayload"){
+    fail("TODO")
+  }
+
+  test("WebHookPullRequestReviewCommentPayload"){
+    fail("TODO")
+  }
+
+  test("WebHookGollumPayload"){
+    fail("TODO")
+  }
+
+}
