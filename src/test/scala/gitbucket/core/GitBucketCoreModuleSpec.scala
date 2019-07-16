b015cdde74dde33dc16b529df24a601d87b017da
Naoki Takezoe <takezoe@gmail.com>
2019-07-16 16:25:47 +0900

    Bump testcontainers to 1.11.4 (#2346)
    * Bump testcontainers to 1.11.4
    * Bump mariadb-java-client to 2.4.2
    * Bump testcontainers-scala to 0.29.0

--- src/test/scala/gitbucket/core/GitBucketCoreModuleSpec.scala
+++ src/test/scala/gitbucket/core/GitBucketCoreModuleSpec.scala
@@ -30,7 +30,7 @@ class GitBucketCoreModuleSpec extends FunSuite {
         override val container = new org.testcontainers.containers.MySQLContainer(s"mysql:$tag") {
           override def getDriverClassName = "org.mariadb.jdbc.Driver"
         }
-        // TODO https://github.com/testcontainers/testcontainers-java/issues/736
+        // TODO https://jira.mariadb.org/browse/CONJ-663
         container.withCommand("mysqld --default-authentication-plugin=mysql_native_password")
       }
       container.start()
