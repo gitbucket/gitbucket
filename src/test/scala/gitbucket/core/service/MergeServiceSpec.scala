738bde377b28986c29604b4886aad45cfb0eec00
Naoki Takezoe <takezoe@gmail.com>
2020-12-28 00:55:21 +0900

    Fix test case

--- src/test/scala/gitbucket/core/service/MergeServiceSpec.scala
+++ src/test/scala/gitbucket/core/service/MergeServiceSpec.scala
@@ -1,18 +1,30 @@
 package gitbucket.core.service
 
-import gitbucket.core.util.Directory._
-import gitbucket.core.util.GitSpecUtil._
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.lib._
 import org.eclipse.jgit.revwalk._
 import org.scalatest.funspec.AnyFunSpec
+import org.mockito.Mockito._
 import java.io.File
-
-import gitbucket.core.plugin.ReceiveHook
-
+import java.util.Date
+import javax.servlet.http.HttpServletRequest
 import scala.util.Using
 
-class MergeServiceSpec extends AnyFunSpec {
+import gitbucket.core.controller.Context
+import gitbucket.core.plugin.ReceiveHook
+import gitbucket.core.util.Directory._
+import gitbucket.core.util.GitSpecUtil._
+import gitbucket.core.service.RepositoryService.RepositoryInfo
+import gitbucket.core.service.SystemSettingsService.SystemSettings
+import gitbucket.core.service.SystemSettingsService.RepositoryOperation
+import gitbucket.core.service.SystemSettingsService.Ssh
+import gitbucket.core.service.SystemSettingsService.WebHook
+import gitbucket.core.service.SystemSettingsService.Upload
+import gitbucket.core.service.SystemSettingsService.RepositoryViewerSettings
+import gitbucket.core.model.Account
+import gitbucket.core.model.RepositoryOptions
+
+class MergeServiceSpec extends AnyFunSpec with ServiceSpecBase {
   val service = new MergeService with AccountService with ActivityService with IssuesService with LabelsService
   with MilestonesService with RepositoryService with PrioritiesService with PullRequestService with CommitsService
   with WebHookPullRequestService with WebHookPullRequestReviewCommentService with RequestCache {
@@ -115,22 +127,94 @@ class MergeServiceSpec extends AnyFunSpec {
   }
   describe("mergePullRequest") {
     it("can merge") {
+      import gitbucket.core.util.Implicits._
+      implicit val context =
+        Context(createSystemSettings(), Some(createAccount("dummy2", "dummy2-fullname", "dummy2@example.com")), request)
+
       val repo8Dir = initRepository("user1", "repo8")
       Using.resource(Git.open(repo8Dir)) { git =>
         createFile(git, s"refs/pull/${issueId}/head", "test.txt", "hoge2")
-        val committer = new PersonIdent("dummy2", "dummy2@example.com")
         assert(getFile(git, branch, "test.txt").content.get == "hoge")
+
         val requestBranchId = git.getRepository.resolve(s"refs/pull/${issueId}/head")
         val masterId = git.getRepository.resolve(branch)
-        service.mergeWithMergeCommit(git, "user1", "repo8", branch, issueId, "merged", committer)(null)
+        val repository = createRepositoryInfo("user1", "repo8")
+
+        withTestDB { implicit session =>
+          service.mergeWithMergeCommit(
+            git,
+            repository,
+            branch,
+            issueId,
+            "merged",
+            context.loginAccount.get,
+            context.settings
+          )
+        }
+
         val lastCommitId = git.getRepository.resolve(branch)
         val commit = Using.resource(new RevWalk(git.getRepository))(_.parseCommit(lastCommitId))
-        assert(commit.getCommitterIdent() == committer)
-        assert(commit.getAuthorIdent() == committer)
+        assert(commit.getCommitterIdent().getName == "dummy2-fullname")
+        assert(commit.getCommitterIdent().getEmailAddress == "dummy2@example.com")
+        assert(commit.getAuthorIdent().getName == "dummy2-fullname")
+        assert(commit.getAuthorIdent().getEmailAddress == "dummy2@example.com")
         assert(commit.getFullMessage() == "merged")
         assert(commit.getParents.toSet == Set(requestBranchId, masterId))
         assert(getFile(git, branch, "test.txt").content.get == "hoge2")
       }
     }
   }
+
+  private def createAccount(userName: String, fullName: String, mailAddress: String): Account =
+    Account(
+      userName = userName,
+      fullName = fullName,
+      mailAddress = mailAddress,
+      password = "password",
+      isAdmin = false,
+      url = None,
+      registeredDate = new Date(),
+      updatedDate = new Date(),
+      lastLoginDate = None,
+      image = None,
+      isGroupAccount = false,
+      isRemoved = false,
+      description = None
+    )
+
+  private def createRepositoryInfo(userName: String, repositoryName: String): RepositoryInfo =
+    RepositoryInfo(
+      owner = userName,
+      name = repositoryName,
+      repository = gitbucket.core.model.Repository(
+        userName = userName,
+        repositoryName = repositoryName,
+        isPrivate = false,
+        description = None,
+        defaultBranch = "master",
+        registeredDate = new Date(),
+        updatedDate = new Date(),
+        lastActivityDate = new Date(),
+        originUserName = None,
+        originRepositoryName = None,
+        parentUserName = None,
+        parentRepositoryName = None,
+        options = RepositoryOptions(
+          issuesOption = "PUBLIC",
+          externalIssuesUrl = None,
+          wikiOption = "PUBLIC",
+          externalWikiUrl = None,
+          allowFork = true,
+          mergeOptions = "merge-commit,squash,rebase",
+          defaultMergeOption = "merge-commit"
+        )
+      ),
+      issueCount = 0,
+      pullCount = 0,
+      forkedCount = 0,
+      milestoneCount = 0,
+      branchList = Nil,
+      tags = Nil,
+      managers = Nil
+    )
 }
