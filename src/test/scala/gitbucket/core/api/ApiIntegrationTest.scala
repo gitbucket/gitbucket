588a91744ea48c0de37964ba3f2894d1e9baf946
Thomas Geier <thomas.geier@solidat.de>
2021-06-28 15:49:19 +0200

    Fix refs API as far as Jenkins github-branch-source plugin can detect tags
    # Conflicts:
    #	src/test/scala/gitbucket/core/api/ApiIntegrationTest.scala

--- src/test/scala/gitbucket/core/api/ApiIntegrationTest.scala
+++ src/test/scala/gitbucket/core/api/ApiIntegrationTest.scala
@@ -134,6 +134,23 @@ class ApiIntegrationTest extends AnyFunSuite {
         assert(statusList.get(1).getState == GHCommitState.FAILURE)
         assert(statusList.get(1).getContext == "context")
       }
+
+      //get master ref
+      {
+        val ref = repo.getRef("heads/master")
+        assert(ref.getRef == "heads/master")
+        assert(ref.getUrl == "tbd")
+        assert(ref.getObject.getType == "commit")
+      }
+
+      //get tag v1.0
+      {
+        val ref = repo.getRef("heads/tags/v1.0")
+        assert(ref.getRef == "heads/tags/v1.0")
+        assert(ref.getUrl == "tbd")
+        assert(ref.getObject.getType == "tag")
+      }
+
     }
   }
 
