119d7d0be047fcbe6b9aad5f34cac0d489a3ca32
Naoki Takezoe <takezoe@gmail.com>
2022-08-28 03:33:19 +0900

    Revert "Drop unused ActivityComponent (#3024)" (#3117)
    This reverts commit bd06e6d4dcbdc095829a4c829778261510ec6c60.

--- src/main/scala/gitbucket/core/model/Activity.scala
+++ src/main/scala/gitbucket/core/model/Activity.scala
@@ -1,5 +1,20 @@
 package gitbucket.core.model
 
+/**
+ * ActivityComponent has been deprecated, but keep it for binary compatibility.
+ */
+@deprecated("ActivityComponent has been deprecated, but keep it for binary compatibility.", "4.34.0")
+trait ActivityComponent extends TemplateComponent { self: Profile =>
+  import profile.api._
+  import self._
+
+  lazy val Activities = TableQuery[Activities]
+
+  class Activities(tag: Tag) extends Table[Activity](tag, "ACTIVITY") with BasicTemplate {
+    def * = ???
+  }
+}
+
 case class Activity(
   userName: String,
   repositoryName: String,
