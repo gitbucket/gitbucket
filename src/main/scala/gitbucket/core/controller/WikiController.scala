gitbucket/gitbucket

commit fbcf962630dea2ac2940d782f3e789ec98e925bd

Check existence of Wiki page for Wiki links (#3907)

src/main/scala/gitbucket/core/view/Markdown.scala
@@ -9,6 +9,7 @@ import gitbucket.core.service.{RepositoryService, RequestCache}
 import gitbucket.core.util.StringUtil
 import io.github.gitbucket.markedj._
 import io.github.gitbucket.markedj.Utils._
+import gitbucket.core.service.WikiService
 
 object Markdown {
 
@@ -75,7 +76,8 @@ object Markdown {
   )(implicit val context: Context)
       extends Renderer(options)
       with LinkConverter
-      with RequestCache {
+      with RequestCache
+      with WikiService {
 
     override def heading(text: String, level: Int, raw: String): String = {
       val id = generateAnchorName(text)
@@ -193,8 +195,11 @@ object Markdown {
           }
         } else {
           // URL is being modified to link to the image file on the repository, but users may want to link to the page if the page name is a link.
-          // As a interim solution, if the link contains a ., it will use the current conversion, otherwise it will perform the conversion the user wants.
-          if (url.lastIndexOf(".") > 0) {
+          // If the wiki page cannot be retrieved from the url, the blob address is returned; otherwise, the page address is returned.
+          val pathElems = context.currentPath.split("/")
+          val owner = pathElems(1)
+          val repos = pathElems(2)
+          if (getWikiPage(owner, repos, url, branch) == None) {
             repository.httpUrl.replaceFirst("/git/", "/").stripSuffix(".git") + "/wiki/_blob/" + url
           } else {
             repository.httpUrl.replaceFirst("/git/", "/").stripSuffix(".git") + "/wiki/" + url

commit f72617769bd555a5fed699c35babfc17d840ba56

Fix potential error in Markdown preview introduced in #3907 (#3967)

src/main/scala/gitbucket/core/controller/WikiController.scala
@@ -103,12 +103,6 @@ trait WikiControllerBase extends ControllerBase {
     }
   })
 
-  private def getWikiBranch(owner: String, repository: String): String = {
-    Using.resource(Git.open(Directory.getWikiRepositoryDir(owner, repository))) { git =>
-      git.getRepository.getBranch
-    }
-  }
-
   get("/:owner/:repository/wiki/:page/_compare/:commitId")(referrersOnly { repository =>
     val pageName = StringUtil.urlDecode(params("page"))
     val Array(from, to) = params("commitId").split("\\.\\.\\.")

src/main/scala/gitbucket/core/service/WikiService.scala
@@ -341,4 +341,18 @@ trait WikiService {
     }
   }
 
+  /**
+    * Get the branch name from the HEAD of the Wiki repository.
+    * 
+    * from gitbucket.core.controller.WikiController
+    *
+    * @param owner Wiki owner
+    * @param repository Wiki repository
+    * @return Branch name
+    */
+  def getWikiBranch(owner: String, repository: String): String = {
+    Using.resource(Git.open(Directory.getWikiRepositoryDir(owner, repository))) { git =>
+      git.getRepository.getBranch
+    }
+  }
 }

src/main/scala/gitbucket/core/view/Markdown.scala
@@ -199,7 +199,7 @@ object Markdown {
           val pathElems = context.currentPath.split("/")
           val owner = pathElems(1)
           val repos = pathElems(2)
-          if (getWikiPage(owner, repos, url, branch) == None) {
+          if (getWikiPage(owner, repos, url, getWikiBranch(owner, repos)) == None) {
             repository.httpUrl.replaceFirst("/git/", "/").stripSuffix(".git") + "/wiki/_blob/" + url
           } else {
             repository.httpUrl.replaceFirst("/git/", "/").stripSuffix(".git") + "/wiki/" + url