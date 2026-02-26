7b598b2bc89f7f02316793443d5e99deb68c3fa7
Yasumichi Akahoshi <sekiseijuju@gmail.com>
2026-02-26 19:32:24 +0900

    Add text completion (use SuggestionProvider)
    - Added ace text completion for text other than markdown.
    - Add emoji completion in markdown

---
+++ src/main/webapp/assets/common/js/emojiCompleter.js
@@ -0,0 +1,32 @@
+(function(window) {
+    function strictNormalizeClassName(rawClassName) {
+        return rawClassName
+            .trim() // Remove the previous and subsequent blanks
+            .toLowerCase() // Unification in lowercase letters
+            .replace(/\s+/g, '-') // Replace whitespace with hyphens
+            .replace(/[^a-z0-9\-_]/g, '_'); // Replace characters other than alphanumeric characters, hyphens, and underscores with underscores
+    }
+
+    if (typeof(emoji) === "object") {
+        window.emojiCompleter = {
+            triggerCharacters: [':'],
+            insertMatch: function (editor, data) {
+                console.log(data);
+            },
+            getCompletions: function (editor, session, pos, prefix, callback) {
+                if (session.$mode && session.$mode.$id === 'ace/mode/markdown') {
+                    callback(null, emoji.map(function (table) {
+                        var token = session.getTokenAt(pos.row, pos.column);
+                        return {
+                            caption: table.description,
+                            value: token ? table.name.replace(token.value, "") : table.name,
+                            className: strictNormalizeClassName(table.description),
+                            meta: 'Emoji'
+                        };
+                    }));
+                }
+            },
+            id: "emojiCompleter"
+        };
+    }
+})(window);
